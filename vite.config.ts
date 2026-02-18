61f4ad46eb34600a876380382d651fb14b81875d
metafit-pixel <netofelipe5050@gmail.com>
2026-02-18 01:54:37 -0300

    Oi

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-  base: '/metafit/',  // Mude 'metafit' para o nome do seu repositório no GitHub
+  plugins: [react(), tailwindcss()],
+  base: '/metafit/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
